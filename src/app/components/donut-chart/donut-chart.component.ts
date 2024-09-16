82718ecf525e617def2baf55ec706511b4a9c41c
hemang-refactors <phemang741@gmail.com>
2024-09-16 14:35:48 +0000

    donut chart labels made dynamic

--- src/app/components/donut-chart/donut-chart.component.ts
+++ src/app/components/donut-chart/donut-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, Input, OnChanges } from '@angular/core';
+import { Component, AfterViewInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import bb, { donut } from 'billboard.js';
 
 @Component({
@@ -13,13 +13,31 @@ export class DonutChartComponent implements AfterViewInit, OnChanges {
   @Input() totalBudget!: number; // in Cr
   @Input() remainingAmount!: number; // in Cr
   @Input() positions: number = 12;
+  labelData: [string, number, string][] = [];
+  
+  colorData: { [key: string]: string } = {
+    "Engineering": "#7AC2B3",
+    "Sales": "#F4C575",
+    "Product": "#A4D1A5",
+    "HR": "#b35600",
+    "Other": "#C1DAD6"
+  };
 
   ngAfterViewInit() {
     this.renderChart();
   }
 
-  ngOnChanges() {
-    this.renderChart(); // Re-render when inputs change
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['tableData'] || changes['usedAmount']) {
+      this.tableData = this.tableData.filter(item => item.name !== 'engineering');
+      const totalValue = this.tableData.reduce((sum, item) => sum + item.y, 0); // Get the total sum
+      this.labelData = this.tableData.map(item => ([
+        item.name,
+        parseFloat(((item.y / totalValue) * 100).toFixed(2)),
+        this.colorData[item.name] || '#FFFFFF'
+      ]));
+      this.renderChart();
+    }
   }
 
   renderChart() {
@@ -27,14 +45,8 @@ export class DonutChartComponent implements AfterViewInit, OnChanges {
       data: {
         columns: this.tableData.map(item => [item.name, item.y]),
         type: donut(),
-        colors: {
-          "Engineering": "#7AC2B3",  // Custom color for Engineering
-          "Sales": "#F4C575",        // Custom color for Sales
-          "Product": "#A4D1A5",      // Custom color for Product
-          "HR": "#b35600",           // Custom color for HR
-          "Other": "#C1DAD6"         // Custom color for Other
-        },
-        labels: false // Disable labels on the chart
+        colors: this.colorData,
+        labels: false
       },
       donut: {
         title: `${this.usedAmount} Cr used`,
